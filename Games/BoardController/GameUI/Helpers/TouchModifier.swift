24b18b0b01d35532be8a5903749ef93358800555
Jo Albright <me@jo2.co>
2021-09-20 15:04:32 -0400

    SwiftUI Conversion

---
+++ Games/BoardController/GameUI/Helpers/TouchModifier.swift
@@ -0,0 +1,37 @@
+//
+//  TouchModifier.swift
+//  Games
+//
+//  Created by Jo Albright on 4/18/21.
+//  Copyright © 2021 Jo Albright. All rights reserved.
+//
+
+import SwiftUI
+
+struct TouchModifier: ViewModifier {
+
+
+//    public func body(content: Content) -> some View {
+//
+//        let tap = TapGesture()
+//
+//        return content.gesture(tap).on
+//
+//    }
+
+//    func coordinate(_ touch: TapGesture) -> Square? {
+//
+//
+//        let w = frame.width / grid.cols.count
+//        let h = frame.height / grid.cols.count
+//
+//        let loc = touch.location(in: self)
+//
+//        let c = Int((loc.x - board.padding) / w)
+//        let r = Int((loc.y - board.padding) / h)
+//
+//        return (r,c)
+//
+//    }
+
+}
